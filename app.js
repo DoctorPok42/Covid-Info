69490c88b23b4074427fc1f9f192e80e76a90ba9
DoctorPok42 <58393254+DoctorPok42@users.noreply.github.com>
2021-04-07 16:00:15 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const Discord = require("discord.js"),
-      MessageAttachment= require('discord.js'),
-      fs = require("fs"),
-      cron = require('node-cron'),
-      Downloader = require('nodejs-file-downloader');
-
+  fs = require("fs"),
+  cron = require("node-cron"),
+  Downloader = require("nodejs-file-downloader");
 
 const client = new Discord.Client();
 client.config = require("./config");
@@ -12,33 +10,44 @@ client.login(client.config.TOKEN);
 
 client.on("ready", () => {
   console.log("Le bot est lancé !");
-  client.user.setPresence({ activity: { name: "Les infos sur le covid", type: 'WATCHING' }, status: 'idle' });
-})
+  client.user.setPresence({
+    activity: { name: "Les infos sur le covid", type: "WATCHING" },
+    status: "idle",
+  });
+});
 
 // définition de la date actuelle
-const datetime = new Date()
-let datefinal = datetime.getDate() + "-" + (datetime.getMonth() + 1) + "-" + datetime.getFullYear() + "-" + datetime.getHours() + "H" + datetime.getMinutes()
+const datetime = new Date();
+let datefinal =
+  datetime.getDate() +
+  "-" +
+  (datetime.getMonth() + 1) +
+  "-" +
+  datetime.getFullYear() +
+  "-" +
+  datetime.getHours() +
+  "H" +
+  (datetime.getMinutes() + 1);
 
 // Ce bout de code ci dessous peut être ajouter au reste pour avoir les minutes et secondes en plus
 //  + datetime.getMinutes() + ":" + datetime.getSeconds()
 
-cron.schedule('50 12,23 * * *', async() => {
+cron.schedule("* 12 * * *", async () => {
+  const downloader = new Downloader({
+    url:
+      "https://disease.sh/v3/covid-19/countries/France?yesterday=true&twoDaysAgo=true&strict=true&allowNull=true", //url du fichier, pour le personaliser -> https://disease.sh/docs/#/
+    directory: "./downloads/", // dossier de stockage
+    fileName: `${datefinal}.json`, // nom du fichier (la date actuelle)
+  });
+  try {
+    await downloader.download();
+    console.log("Fichier Télécharger !\n" + datefinal);
+  } catch (error) {
+    console.log("Téléchargement échoué\n" + datefinal, error);
+  }
 
-      const downloader = new Downloader({
-        url: 'https://disease.sh/v3/covid-19/countries/France?yesterday=true&twoDaysAgo=true&strict=true&allowNull=true', //url du fichier, pour le personaliser -> https://disease.sh/docs/#/
-        directory: "./downloads/", // dossier de stockage
-        fileName:`${datefinal}.json` // nom du fichier (la date actuelle)
-      })
-      try {
-        await downloader.download();
-        console.log('Fichier Télécharger !\n' + datefinal);
-      } catch (error) {
-        console.log('Téléchargement échoué\n' + datefinal,error)
-      }
-
-
-      // Envoie du fichier sur un channel Discord [OPTIONAL]
-      const buffer = fs.readFileSync(`./downloads/${datefinal}.json`);
-      const attachment = new MessageAttachment(buffer, `${datefinal}.json`);
-      client.channels.cache.get('CHANNELID').send(attachment)
+  // Envoie du fichier sur un channel Discord [OPTIONAL]
+  const buffer = fs.readFileSync(`./downloads/${datefinal}.json`);
+  const attachment = new Discord.MessageAttachment(buffer, `${datefinal}.json`);
+  client.channels.cache.get("CHANNELID").send(attachment);
 });
